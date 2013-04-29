97ed5f392b8f8d35242bcdae9bbcd195eb02023e
Cody+Natasha <Cody+Natasha@appacademy.io>
2013-04-29 11:07:57 -0400

    two sum tests added

--- spec/arrays_spec.rb
+++ spec/arrays_spec.rb
@@ -15,9 +15,30 @@ describe Array do
 			[1,2,3,1].my_uniq.should == [1,2,3]
 		end
 
-		# it "should not call uniq" do
-# 			A
-# 		end
-#
+		it "should not call uniq" do
+			should_not_receive(:uniq)
+		end
+	end
+
+	describe "#two_sum?" do
+		it "returns false on empty array" do
+			[].two_sum?.should == false
+		end
+
+		it "returns false if size < 2" do
+			[0].two_sum?.should == false
+		end
+
+		it "returns false if no two sum to zero" do
+			[1,2,3].two_sum?.should == false
+		end
+
+		it "returns true if two numbers sum to zero" do
+			[1,2,3,-2].two_sum?.should == true
+		end
+
+		it "returns false if has only one zero" do
+			[1,2,3,0].two_sum?.should == false
+		end
 	end
 end
