73b7e8f8055d00ea87056337b0afdf531755ec3b
MCodyB <mcodyb@gmail.com>
2013-04-30 08:25:12 -0300

    Update reversi_spec.rb

--- spec/reversi_spec.rb
+++ spec/reversi_spec.rb
@@ -2,7 +2,17 @@ require 'rspec'
 require 'reversi'
 
 describe Board do
+  let(:board) {Board.new}
+	describe "#initialize"
+	it "returns a grid with 8 rows" do
+		board.grid.size.should == 8
+	end
 
+	it "has rows 8 long" do
+		board.grid[rand(0..7)].size.should == 8
+	end
+
+	it "places"
 end
 
 describe Piece do
